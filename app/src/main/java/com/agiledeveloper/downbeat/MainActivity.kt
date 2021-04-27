f394b8ef55b004501123aa4664a0fc2d723e0c83
BecBarnett <s3856827@student.rmit.edu.au>
2021-04-27 18:42:35 +1000

    DownBeat Default Activity

---
+++ app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agiledeveloper.downbeat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// DownBeat Inception
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
