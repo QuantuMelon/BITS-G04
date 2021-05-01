1cfc11bc1da2dc5a46b55dc8f57575e6ca2dd2de
BecBarnett <s3856827@student.rmit.edu.au>
2021-05-01 12:04:46 +1000

    Added action listener for Login button.  Now directs to events page.

--- app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
+++ app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
@@ -2,7 +2,8 @@ package com.agiledeveloper.downbeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.ImageButton
 
 
 // DownBeat Inception
@@ -10,6 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Login button
+        val loginButton = findViewById<ImageButton>(R.id.btnLogin)
+
+        //Handle button tap
+        loginButton.setOnClickListener { setContentView(R.layout.event_page) }
+
     }
 }
 //Declan
