3399b3c3b96147f85e2838837db5461ebbaf4c28
BecBarnett <s3856827@student.rmit.edu.au>
2021-05-01 13:29:34 +1000

    Added Hamburger Menu.  Work in progress still, would like to maybe make elements on the page visible when clicked in order to make menu functional.  May be simpler to use a menu view xml file.

--- app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
+++ app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.agiledeveloper.downbeat
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-
+import android.view.MenuItem;
+import android.widget.Toast;
 
 // DownBeat Inception
 class MainActivity : AppCompatActivity() {
@@ -14,11 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         //Login button
         val loginButton = findViewById<ImageButton>(R.id.btnLogin)
+        val menuButton = findViewById<ImageButton>(R.id.btnMenu)
 
         //Handle button tap
         loginButton.setOnClickListener { setContentView(R.layout.event_page) }
-
     }
+
+
 }
 //Declan
 //Gregory
