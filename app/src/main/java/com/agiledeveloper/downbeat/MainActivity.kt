0c17580f129ce5b37938ea6ffc5d65a1aeca856c
BecBarnett <s3856827@student.rmit.edu.au>
2021-05-02 14:57:12 +1000

    Navigation overhauled.  Hamburger working.  Bottom Navigation Working.  Some cleanup still to do.

--- app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
+++ app/src/main/java/com/agiledeveloper/downbeat/MainActivity.kt
@@ -1,44 +1,51 @@
 package com.agiledeveloper.downbeat
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.view.MenuItem;
-import android.widget.TextView
-import android.widget.Toast;
 
-// DownBeat Inception
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Login button
-        val loginButton = findViewById<ImageButton>(R.id.btnLogin)
-
-
-        //Handle button tap
-        loginButton.setOnClickListener {
-            setContentView(R.layout.event_page)
-            eventsPage()
-        }
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        /*val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }*/
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_events, R.id.nav_maps, R.id.nav_createevents
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
     }
 
-    //Moving everything EventsPage related into it's own function for now
-    //Avoids error where setting an OnClickListener for an object not on current page crashes app
-    fun eventsPage(){
-        title = "Events Page"
-        val eventButton = findViewById<TextView>(R.id.btnEvents)
-        //Handle Events Button Click
-        eventButton.setOnClickListener {
-            setContentView(R.layout.activity_main)
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
-//Declan
-//Gregory
-//Brandon
-//Rebecca
