298bc1a32a1c126a395f721d5cdcb3a067b90551
nickmpan <s3863803@student.rmit.edu.au>
2021-05-14 00:00:25 +1000

    Changed permissions for API - updated Manifest and maps fragments

--- app/src/main/java/com/agiledeveloper/downbeat/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/agiledeveloper/downbeat/ui/maps/MapsFragment.kt
@@ -13,14 +13,17 @@ class MapsFragment : AppCompatActivity() {
     lateinit var mapFragment: SupportMapFragment
     lateinit var googleMap: GoogleMap
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
             setContentView(R.layout.fragment_maps)
 
-            mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-            mapFragment.getMapAsync(OnMapReadyCallback {
-                googleMap = it
-            })
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+        })
     }
 }
+
+
+
 
